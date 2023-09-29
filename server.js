f4147fe7356da54945c7d6109f48e6477006c07b
Jilmer Padua <jiis.padua.up@phinamed.com>
2023-09-29 20:31:32 +0800

    mongoose connection message fixed

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const authRoutes = require('./router/auth');
+const productRoutes = require('./router/productRoutes')
 const cors = require('cors');
 
 const app = express();
@@ -15,13 +16,19 @@ app.use(bodyParser.json());
 
 // Routes
 app.use('/auth', authRoutes);
+app.use('/product', productRoutes);
 
-// MongoDB connection
+// Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
-
+.then(() => {
+  console.log('Connected to MongoDB');
+})
+.catch((err) => {
+  console.error('Error connecting to MongoDB:', err.message);
+});
 
 // Start the server
 const port = process.env.PORT || 3000;
