c2662ee00eb0484f54189b023046341e9bdbdb72
Jilmer Padua <jiis.padua.up@phinamed.com>
2023-10-04 09:22:41 +0800

    server.js fully restored

--- server.js
+++ server.js
@@ -1,34 +1,35 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const authRoutes = require('./router/auth');
-const productRoutes = require('./router/productRoutes')
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const authRoutes = require("./router/auth");
+const productRoutes = require("./router/productRoutes");
+const cors = require("cors");
 
 const app = express();
 
 // Load environment variables from .env file
-require('dotenv').config();
+require("dotenv").config();
 
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
 
 // Routes
-app.use('/auth', authRoutes);
-app.use('/product', productRoutes);
-
+app.use("/auth", authRoutes);
+app.use("/product", productRoutes);
+app.use("/uploads", express.static("uploads"));
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => {
-  console.log('Connected to MongoDB');
-})
-.catch((err) => {
-  console.error('Error connecting to MongoDB:', err.message);
-});
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err.message);
+  });
 
 // Start the server
 const port = process.env.PORT || 3000;
