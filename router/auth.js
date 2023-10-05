fbd3dd3dfe02236531ec370a90d801565b0c4509
AustineBaltazar <AustineBaltazar.main@gmail.com>
2023-10-05 19:08:51 +0800

    added a product

--- router/auth.js
+++ router/auth.js
@@ -1,30 +1,28 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const User = require('../models/User');
-const nodemailer = require('nodemailer');
-const crypto = require('crypto');
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+const nodemailer = require("nodemailer");
+const crypto = require("crypto");
 
 // Load environment variables from .env file
-require('dotenv').config();
+require("dotenv").config();
 
 // JWT secret key
 const jwtSecret = process.env.JWT_SECRET;
 
 // Create a transporter with Gmail SMTP settings
 const transporter = nodemailer.createTransport({
-  service: 'Gmail',
+  service: "Gmail",
   auth: {
     user: process.env.GMAIL_EMAIL,
     pass: process.env.GMAIL_PASSWORD,
   },
 });
 
-  
-
 // Register new user
-router.post('/signup', async (req, res) => {
+router.post("/signup", async (req, res) => {
   try {
     const { email, password } = req.body;
 
@@ -32,7 +30,7 @@ router.post('/signup', async (req, res) => {
     const existingUser = await User.findOne({ email });
 
     if (existingUser) {
-      return res.status(400).json({ message: 'User already exists' });
+      return res.status(400).json({ message: "User already exists" });
     }
 
     // Hash the password
@@ -45,52 +43,52 @@ router.post('/signup', async (req, res) => {
 
     await user.save();
 
-    res.status(201).json({ message: 'User registered successfully' });
+    res.status(201).json({ message: "User registered successfully" });
   } catch (error) {
-    res.status(500).json({ message: 'Internal server error' });
+    res.status(500).json({ message: "Internal server error" });
   }
 });
 
 // User login
-router.post('/login', async (req, res) => {
+router.post("/login", async (req, res) => {
   try {
     const { email, password } = req.body;
 
     const user = await User.findOne({ email });
 
     if (!user) {
-      return res.status(400).json({ message: 'Invalid credentials' });
+      return res.status(400).json({ message: "Invalid credentials" });
     }
 
     const isPasswordValid = await bcrypt.compare(password, user.password);
 
     if (!isPasswordValid) {
-      return res.status(400).json({ message: 'Invalid credentials' });
+      return res.status(400).json({ message: "Invalid credentials" });
     }
 
     const token = jwt.sign({ userId: user._id }, jwtSecret, {
-      expiresIn: '1h',
+      expiresIn: "1h",
     });
 
     res.json({ token });
   } catch (error) {
-    console.error('Error:', error);
-    res.status(500).json({ message: 'Internal server error' });
+    console.error("Error:", error);
+    res.status(500).json({ message: "Internal server error" });
   }
 });
 
 // Forgot password
-router.post('/forgot-password', async (req, res) => {
+router.post("/forgot-password", async (req, res) => {
   try {
     const { email } = req.body;
 
     const user = await User.findOne({ email });
 
     if (!user) {
-      return res.status(400).json({ message: 'User not found' });
+      return res.status(400).json({ message: "User not found" });
     }
 
-    const token = crypto.randomBytes(20).toString('hex');
+    const token = crypto.randomBytes(20).toString("hex");
     const resetPasswordExpires = Date.now() + 3600000; // 1 hour
 
     user.resetPasswordToken = token;
@@ -102,11 +100,12 @@ router.post('/forgot-password', async (req, res) => {
     const mailOptions = {
       to: user.email,
       from: process.env.GMAIL_EMAIL,
-      subject: 'Password Reset',
-      text: `You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n`
-        + `Please click on the following link, or paste this into your browser to complete the process:\n\n`
-        + `${process.env.CLIENT_URL}/reset-password/${token}\n\n`
-        + `If you did not request this, please ignore this email, and your password will remain unchanged.\n`,
+      subject: "Password Reset",
+      text:
+        `You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n` +
+        `Please click on the following link, or paste this into your browser to complete the process:\n\n` +
+        `${process.env.CLIENT_URL}/reset-password/${token}\n\n` +
+        `If you did not request this, please ignore this email, and your password will remain unchanged.\n`,
     };
 
     // Send the email using OAuth 2.0 authentication
@@ -114,16 +113,15 @@ router.post('/forgot-password', async (req, res) => {
       if (err) {
         return res.status(500).json({ message: err.message });
       }
-      res.json({ message: 'Password reset email sent' });
+      res.json({ message: "Password reset email sent" });
     });
   } catch (error) {
-    res.status(500).json({ message: 'Internal server error' });
+    res.status(500).json({ message: "Internal server error" });
   }
 });
 
-
 // Reset password
-router.post('/reset-password', async (req, res) => {
+router.post("/reset-password", async (req, res) => {
   try {
     const { token, newPassword } = req.body;
 
@@ -133,7 +131,9 @@ router.post('/reset-password', async (req, res) => {
     });
 
     if (!user) {
-      return res.status(400).json({ message: 'Token is invalid or has expired' });
+      return res
+        .status(400)
+        .json({ message: "Token is invalid or has expired" });
     }
 
     const hashedPassword = await bcrypt.hash(newPassword, 10);
@@ -144,45 +144,45 @@ router.post('/reset-password', async (req, res) => {
 
     await user.save();
 
-    res.json({ message: 'Password reset successfully' });
+    res.json({ message: "Password reset successfully" });
   } catch (error) {
-    console.error('Error:', error);
-    res.status(500).json({ message: 'Internal server error' });
+    console.error("Error:", error);
+    res.status(500).json({ message: "Internal server error" });
   }
 });
 
-  
-  // Change password
-router.post('/change-password', async (req, res) => {
-    try {
-      const { email, currentPassword, newPassword } = req.body;
-  
-      // Find the user by their email
-      const user = await User.findOne({ email });
-  
-      if (!user) {
-        return res.status(404).json({ message: 'User not found' });
-      }
-  
-      // Check if the current password provided matches the stored password
-      const isPasswordValid = await bcrypt.compare(currentPassword, user.password);
-  
-      if (!isPasswordValid) {
-        return res.status(400).json({ message: 'Current password is incorrect' });
-      }
-  
-      // Hash the new password and update it in the database
-      const hashedPassword = await bcrypt.hash(newPassword, 10);
-      user.password = hashedPassword;
-      await user.save();
-  
-      res.json({ message: 'Password changed successfully' });
-    } catch (error) {
-      console.error('Error:', error);
-      res.status(500).json({ message: 'Internal server error' });
+// Change password
+router.post("/change-password", async (req, res) => {
+  try {
+    const { email, currentPassword, newPassword } = req.body;
+
+    // Find the user by their email
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
     }
-  });
-  
-  
+
+    // Check if the current password provided matches the stored password
+    const isPasswordValid = await bcrypt.compare(
+      currentPassword,
+      user.password
+    );
+
+    if (!isPasswordValid) {
+      return res.status(400).json({ message: "Current password is incorrect" });
+    }
+
+    // Hash the new password and update it in the database
+    const hashedPassword = await bcrypt.hash(newPassword, 10);
+    user.password = hashedPassword;
+    await user.save();
+
+    res.json({ message: "Password changed successfully" });
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
 
 module.exports = router;
