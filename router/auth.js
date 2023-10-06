50552759ca0f2b9f8599f31a22885dabb952b463
Jilmer Padua <jiis.padua.up@phinamed.com>
2023-10-06 16:49:48 +0800

    populate staffProfile

--- router/auth.js
+++ router/auth.js
@@ -68,8 +68,7 @@ router.post("/login", async (req, res) => {
   try {
     const { email, password, staffProfile } = req.body;
 
-    const user = await User.findOne({ email });
-    const profile = await User.findOne({ staffProfile });
+    const user = await User.findOne({ email }).populate('staffProfile');
 
     if (!user) {
       return res.status(400).json({ message: "Invalid credentials" });
@@ -85,7 +84,7 @@ router.post("/login", async (req, res) => {
     req.session.userId = user._id;
 
     // Send success response
-    res.json({ message: "Login successful", profile });
+    res.json({ message: "Login successful" , staffProfile});
   } catch (error) {
     console.error("Error:", error);
     res.status(500).json({ message: "Internal server error" });
