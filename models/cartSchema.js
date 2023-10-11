8fd0d4b3e4cb8a1e399b980774b1af080ef5c566
Jilmer Padua <jiis.padua.up@phinamed.com>
2023-10-11 10:18:56 +0800

    added quantiy and totalPrice to cartSchema

--- models/cartSchema.js
+++ models/cartSchema.js
@@ -2,6 +2,13 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const cartSchema = new mongoose.Schema({
+    quantity: {
+        type: Number,
+        default: 1
+    },
+    totalPrice: {
+        type: Number,
+    },
     cart: [{
         type: Schema.Types.ObjectId,
         ref: 'Product'
